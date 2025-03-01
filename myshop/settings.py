eebba42f266e8eb4e344ffc331c07ae487ecbe51
motiefard <mozhdemotieifard@gmail.com>
2025-03-01 22:43:38 +0330

    build a shopping cart: add cart-app

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'        # for uploading
 MEDIA_ROOT = BASE_DIR / 'media'     # local path which files live
+
+
+CART_SESSION_ID = 'cart'
