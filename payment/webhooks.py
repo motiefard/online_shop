02dfbc9a4a52d757291bc2b04899e21444f1aa46
motiefard <mozhdemotieifard@gmail.com>
2025-04-25 18:13:04 +0330

    webhook : add stripe_id to Order, update it in webhook

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -39,6 +39,10 @@ def stripe_webhook(request):
             
             # mark order as paid
             order.paid = True
+
+            # store Stripe payment ID
+            order.stripe_id = session.payment_intent
+
             order.save()
 
     return HttpResponse(status=200)
