404a98f27411b49443427423cfdb3b630c6013fc
motiefard <mozhdemotieifard@gmail.com>
2025-04-25 17:57:25 +0330

    webhook : create webhook-endpoint to receive payment notification for event:checkout.session.completed from Stripe

--- payment/views.py
+++ payment/views.py
@@ -26,7 +26,7 @@ def payment_process(request):
             'client_reference_id': order.id,
             'success_url': success_url,
             'cancel_url': cancel_url,
-            'line_items': []
+            'line_items': [],
         }
 
         # add order items to the Stripe checkout session
@@ -46,8 +46,10 @@ def payment_process(request):
 
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
+
         # redirect to Stripe payment form
         return redirect(session.url, code=303)
+
     else:
         return render(request, 'payment/process.html', locals())
 
